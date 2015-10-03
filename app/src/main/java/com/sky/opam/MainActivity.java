8541cc381dd449d55ee33892d421ef4d8e30add5
Li <helene.li.xu@gmail.com>
2015-10-03 17:55:25 +0200

    [DEV] menu manager init

--- app/src/main/java/com/sky/opam/MainActivity.java
+++ app/src/main/java/com/sky/opam/MainActivity.java
@@ -3,12 +3,11 @@ package com.sky.opam;
 import android.os.Bundle;
 import android.os.Environment;
 
-import com.loic.common.LibApplication;
 import com.loic.common.dynamicLoad.PluginApk;
 import com.loic.common.dynamicLoad.PluginManager;
 import com.loic.common.fragManage.GcActivity;
 import com.loic.common.fragManage.GcFragment;
-import com.loic.common.fragManage.MenuCreater;
+import com.loic.common.fragManage.MenuManager;
 import com.sky.opam.fragment.AgendaViewFragment;
 import com.sky.opam.fragment.SettingFragment;
 import com.sky.opam.fragment.TrombiFragment;
@@ -27,29 +26,5 @@ public class MainActivity extends GcActivity
         PluginApk pluginApk = pluginManager.getPluginApk(pluginFile.listFiles()[0].getAbsolutePath());
 
         setCenterFragment(pluginApk.getPluginFragment());
-    }
-
-    @Override
-    protected Class<? extends GcFragment> getInitCenterFragment() 
-    {
-        return null;
-    }
-
-    @Override
-    protected void initMenuItem(MenuCreater menuCreater) 
-    {
-        menuCreater.appendAutoAppMenuSection()
-        .appendMenuElement(getString(R.string.OA0000), AgendaViewFragment.class, android.R.drawable.ic_menu_agenda)
-        .appendMenuElement(getString(R.string.OA0001), TrombiFragment.class, android.R.drawable.ic_menu_search)
-        .appendMenuElement(getString(R.string.OA0002), SettingFragment.class, android.R.drawable.ic_menu_manage)
-        .appendMenuElement(getString(R.string.OA0003), null, android.R.drawable.ic_menu_close_clear_cancel);
-    }
-
-    private Class<? extends GcFragment> getPluginFragClass()
-    {
-        PluginManager pluginManager = PluginManager.getInstance();
-        File pluginFile = new File(Environment.getExternalStorageDirectory(), "plugin");
-        PluginApk pluginApk = pluginManager.getPluginApk(pluginFile.listFiles()[0].getAbsolutePath());
-        return  pluginApk.getPluginFragmentClass();
     }
 }
